35bf37b2372d3376abb27f13d55fb53784e905d1
petrus766hi <petrus.sigiro@gmail.com>
2023-06-26 13:15:15 +0700

    test

---
+++ src/main/java/com/juaracoding/pojavafundamental/testharian/testarray/TestArray.java
@@ -0,0 +1,21 @@
+package com.juaracoding.pojavafundamental.testharian.testarray;
+/*
+IntelliJ IDEA 2023.1.2 (Community Edition)
+Build #IC-231.9011.34, built on May 16, 2023
+@Author petrus a.k.a. Petrus
+Java Developer
+Created on 26/06/23 13.13
+@Last Modified 26/06/23 13.13
+Version 1.0
+*/
+
+public class TestArray {
+    public static void main(String[] args) {
+        String strx = "OK";
+        if(!strx.equals("OK")) {
+            System.out.println("SUKSES");
+        } else {
+            System.out.println("TIDAK SUKSES");
+        }
+    }
+}
