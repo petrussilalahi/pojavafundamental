e3017f2a3d06cc1364dbf46ddc1ab32c5f2a2609
petrus766hi <petrus.sigiro@gmail.com>
2023-06-28 21:07:19 +0700

    done

---
+++ src/main/java/com/juaracoding/pojavafundamental/cobacoba/MethodSatu.java
@@ -0,0 +1,29 @@
+package com.juaracoding.pojavafundamental.cobacoba;
+/*
+IntelliJ IDEA 2023.1.2 (Community Edition)
+Build #IC-231.9011.34, built on May 16, 2023
+@Author petrus a.k.a. Petrus
+Java Developer
+Created on 28/06/23 20.53
+@Last Modified 28/06/23 20.53
+Version 1.0
+*/
+
+public class MethodSatu {
+    public static void main(String[] args) {
+        System.out.println(hitungLuasPersegi(5));
+        System.out.println(hitungLuasLayangLayang(5.3, 100));
+    }
+
+    public static double hitungLuasPersegi(double dobs1)
+    {
+        double dobresult = dobs1 * dobs1;
+        return dobresult;
+    }
+
+    public static double hitungLuasLayangLayang(double dobs2, double dobs3)
+    {
+        double dobresultLayang = (dobs2 * dobs3) / 2;
+        return dobresultLayang;
+    }
+}
